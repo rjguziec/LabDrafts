eeeb07a11aba498f3bf6571c17d6d5847daac11e
Robert G. <guziecr@dupage.edu>
2025-02-20 11:12:12 -0600

    Add files via upload

--- Lab3DraftMux.ino
+++ Lab3DraftMux.ino
@@ -12,13 +12,13 @@
   A4:
   A5:
   D0:
-  D1: a
-  D2: b
-  D3: c 
-  D4: d
-  D5: e 
-  D6: f
-  D7: g
+  D1: SEG G
+  D2: SEG F
+  D3: SEG E
+  D4: SEG D
+  D5: SEG C
+  D6: SEG B
+  D7: SEG A
   D8:  MUX1
   D9: MUX2
   D10:MUX3
@@ -29,13 +29,14 @@
 
 void setup()
 {
+  // Setup for our ADC 
   ADCSRA = 0xE7;
   ADCSRB = 0x00;
   ADMUX = 0x40;
 
+  // Setup for our output registers
   DDRD = 0xFE;
   DDRB = 0x0F;
-
 
 }
 
@@ -44,11 +45,14 @@ void loop()
   // Read Pot
   unsigned int adcr = ADC;
 
+  // Define constants for our indexs and arrays
   const unsigned char n = 4;
   const unsigned char m = 10;
 
+  // Mux arrive to write which Anode is active (ACTIVE LOW)
   unsigned char muxArray[n] = {0xF7, 0xFB, 0xFD, 0xFE};
 
+  // Number arrive to write which segments are to be written
   unsigned char numArray[m] = {0xFC, 0x60, 0xDA, 0xF2, 0x66, 0xB6, 0xBE, 0xE4, 0xFE, 0xF6};
 
   // Read ones place of ADC
@@ -60,18 +64,26 @@ void loop()
   // Read thousands place of ADC
   unsigned int thousPlace = (adcr / 1000 );
 
+  // Array for which place to pull up
   unsigned char placeArray[n] = {onePlace, tenPlace, hundPlace, thousPlace};
 
   for (unsigned int i = 0; i < n; i++) {
 
+    // Selective clear and go through mux values
     PORTB &= 0xF0;
     PORTB |= muxArray[i];
 
+    // Selective clear and decipher numeric placce and value!
     PORTD &= 0x01;
     PORTD |= numArray[placeArray[i]];
     _delay_us(1000);
   }
 
+
+
+
+
+  // DISREGARD THE REST OF THIS, THIS WAS PRIMARILY FOR TESTING PURPOSES :D
   //PORTB &= 0xF0;
   //PORTB |= 0xF7;
   
